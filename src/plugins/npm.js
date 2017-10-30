78294039faa6db6f2097aab3e9122488888e164e
IntPtr <lxy.lixiaoyan@gmail.com>
2017-10-30 20:35:53 +0800

    add npm/search

--- src/plugins/npm.js
+++ src/plugins/npm.js
@@ -9,10 +9,7 @@ export default app => {
   };
 
   app.hears(/^'npm /, async ctx => {
-    const name = ctx.message.text
-      .replace(/^'npm /, "")
-      .trim()
-      .toLowerCase();
+    const name = ctx.message.text.split(/\s+/, 2)[1];
 
     let info;
     try {
@@ -22,9 +19,10 @@ export default app => {
     } catch (err) {
       if (err.response.status === 404) {
         await ctx.reply("Package not found.");
-        return;
+      } else {
+        await ctx.reply(`Network error: ${err.response.status}.`);
       }
-      throw err;
+      return;
     }
 
     const link = `https://www.npmjs.com/package/${info.name}`;
@@ -41,7 +39,40 @@ export default app => {
     );
   });
 
+  app.hears(/^'npm\/search /, async ctx => {
+    const pattern = ctx.message.text.split(/\s+/, 2)[1];
+
+    let result;
+    try {
+      result = (await axios.get(
+        `https://registry.npmjs.org/-/v1/search?text=${encodeURIComponent(
+          pattern,
+        )}&size=5`,
+      )).data;
+    } catch (err) {
+      await ctx.reply(`Network error: ${err.response.status}.`);
+      return;
+    }
+
+    await ctx.reply(
+      [
+        `<b>npm/search</b> › ${escape(pattern)}`,
+        "┄┄",
+        ...result.objects.map(
+          item =>
+            `<b>${escape(item.name)}</b>` + item.description
+              ? ` › ${escape(item.description)}`
+              : "",
+        ),
+      ].join("\n"),
+      { parse_mode: "HTML", disable_web_page_preview: true },
+    );
+  });
+
   return {
-    help: "<b>'npm &lt;package&gt;</b> › show package info",
+    help: [
+      "<b>'npm &lt;package&gt;</b> › show package info",
+      "<b>'npm/search &lt;pattern&gt;</b> › search packages",
+    ].join("\n"),
   };
 };
